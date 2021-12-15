539d367b017c8bb3b42714b1d1f1db7c455ddded
KaioGerhardt <gerhardt.kaio@gmail.com>
2021-12-15 15:26:56 -0300

    Update 5-aprovacao.py

--- 2-EstruturaDeDecisao/5-aprovacao.py
+++ 2-EstruturaDeDecisao/5-aprovacao.py
@@ -4,6 +4,7 @@ média alcançada por aluno e apresentar:
 - A mensagem "Reprovado", se a média for menor do que sete;
 - A mensagem "Aprovado com Distinção", se a média for igual a dez."""
 
+#------------------- Código -------------------
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 
